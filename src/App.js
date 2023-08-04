e66483116553b63e4deb9f9c3cb33741f409f95e
Kathryn <richardson.kathryn@outlook.com>
2023-08-04 14:39:32 -0400

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Board from './Board.jsx';
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flexbox-container'>
+      <div className='board'>
+        <h1>Kat's Tic-Tac-Toe App</h1>
+        <Board />
+      </div>
     </div>
   );
 }
